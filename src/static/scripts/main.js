cda19f3751c5950cfdf70395f1bfa43603cc916b
kairat <mailforstudy.kg@gmail.com>
2019-01-13 23:24:05 +0600

    finish

--- src/static/scripts/main.js
+++ src/static/scripts/main.js
@@ -23,17 +23,31 @@ document.addEventListener('DOMContentLoaded', function() {
             }
             if ($slider_video) {
                 $($slider_video).owlCarousel({
-                    items: 2,
+                    items: 1,
                     loop: true,
                     margin: 10,
                     video: true,
                     lazyLoad: false,
+                     responsive:{
+                        580:{
+                            items: 2
+                        }
+                    }
                 });
             }
         })();
 
         (function() {
             $(".main-menu").on("click","a", function (event) {
+                event.preventDefault();
+                var id  = $(this).attr('href'),
+                //узнаем высоту от начала страницы до блока на который ссылается якорь
+                top = $(id).offset().top;
+                $('body,html').animate({scrollTop: top}, 1500);
+            });
+        })();
+        (function() {
+            $(".mobile-menu").on("click","a", function (event) {
                 event.preventDefault();
                 var id  = $(this).attr('href'),
                 //узнаем высоту от начала страницы до блока на который ссылается якорь
@@ -86,65 +100,46 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         })();
 
-
-    // var swiper = new Swiper('.swiper-container', {
-    //   slidesPerView: 4,
-    //   freeMode: true,
-    //   loop: true,
-    //   breakpoints: {
-    //       1280: {
-    //         slidesPerView: 3,
-    //       },
-    //       920: {
-    //         slidesPerView: 2,
-    //       },
-    //       720: {
-    //         slidesPerView: 1
-    //         }
-    //     }
-    // });
-
-
     // jQuery - Мобильное меню
-    // var mobileMenu = (function() {
-    //     var $trigger = $('.mobile-menu__arrow');
-    //     var animationSpeed = 200;
+    var mobileMenu = (function() {
+        var $trigger = $('.mobile-menu__arrow');
+        var animationSpeed = 200;
 
-    //     // Mobile menu
-    //     $trigger.on('click', function() {
-    //         toggleMenu($(this));
-    //     });
+        // Mobile menu
+        $trigger.on('click', function() {
+            toggleMenu($(this));
+        });
 
-    //     function toggleMenu(el) {
-    //         var li = el.closest('li');
+        function toggleMenu(el) {
+            var li = el.closest('li');
 
-    //         el.toggleClass('is-active');
-    //         li.toggleClass('is-active');
+            el.toggleClass('is-active');
+            li.toggleClass('is-active');
 
-    //         li.children('ul').slideToggle(animationSpeed, function() {
-    //             li.children('ul').toggleClass('is-opened');
-    //         });
-    //     }
-    // })();
+            li.children('ul').slideToggle(animationSpeed, function() {
+                li.children('ul').toggleClass('is-opened');
+            });
+        }
+    })();
 
     // Открытье и закрытье mobile-menu
-    // (function(){
+    (function(){
 
-    //     var $mobile_open = document.querySelector('.js-mobile-panel-open');
-    //     var $mobile_close = document.querySelector('.js-mobile-panel-close');
-    //     var $mobile_pane = document.querySelector('.js-mobile-panel');
-    //     var $base = document.documentElement;
+        var $mobile_open = document.querySelector('.js-mobile-panel-open');
+        var $mobile_close = document.querySelector('.js-mobile-panel-close');
+        var $mobile_pane = document.querySelector('.js-mobile-panel');
+        var $base = document.documentElement;
 
-    //     $mobile_open.addEventListener('click', openPanel);
-    //     $mobile_close.addEventListener('click', closePanel);
+        $mobile_open.addEventListener('click', openPanel);
+        $mobile_close.addEventListener('click', closePanel);
 
-    //     function openPanel(){
-    //         $mobile_pane.classList.add('is-opened');
-    //         $base.style.overflow = 'hidden';
-    //     };
-    //     function closePanel(){
-    //         $mobile_pane.classList.remove('is-opened');
-    //         $base.style.overflow = '';
-    //     };
-    // })();
+        function openPanel(){
+            $mobile_pane.classList.add('is-opened');
+            $base.style.overflow = 'hidden';
+        };
+        function closePanel(){
+            $mobile_pane.classList.remove('is-opened');
+            $base.style.overflow = '';
+        };
+    })();
 });
